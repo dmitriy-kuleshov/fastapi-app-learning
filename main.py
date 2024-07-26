4dd12f312500b5cfc68d957a3353dbaf92049e9a
dimitrius <dimakyl0306@gmail.com>
2024-07-26 22:28:36 +0300

    добавил запрос

--- main.py
+++ main.py
@@ -25,8 +25,26 @@ async def get_model(model_name: ModelName):
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+fake_items = [{"item_name": "barsa"}, {"item_name": "arsenal"}, {"item_name": "liverpool"},
+              {"item_name": "zenit"}, {"item_name": "cska"}, {"item_name": "real"},
+              {"item_name": "manchester"}, {"item_name": "tottenham"}, {"item_name": "juventus"},
+              {"item_name": "liverpool"}, {"item_name": "out of limit"}
+              ]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):  # ограничения для количества элементов массива
+    return fake_items[skip: skip + limit]
 
 
 @app.get("/users/{user_id}")
